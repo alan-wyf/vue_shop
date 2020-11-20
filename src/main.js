cfa9ec24e4192f38f2727ace1f89a7aa743914cb
alan-wyf <wwwyf0817@gmail.com>
2020-11-21 01:41:12 +0800

    login

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import './plugins/element.js'
 import './assets/fonts/iconfont.css'
 import './assets/css/global.css'
+
 import axios from 'axios'
 
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
